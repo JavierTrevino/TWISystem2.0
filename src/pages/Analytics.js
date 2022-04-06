0045766e889a9de3c497429fe19943f39337ef27
JavierTrevino <A00828902@itesm.mx>
2022-04-06 18:02:49 -0500

    Javier

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import NavBarA from '../components/NavBarA';
+import React, { Component } from "react";
+import NavBarA from "../components/NavBarA";
 
 export default class Analytics extends Component {
-    render() {
-        return (
-            <div>
-                <NavBarA />
-                <h1>Analytics</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <NavBarA />
+        <h1>Analytics</h1>
+        <div></div>
+      </div>
+    );
+  }
 }
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import NavBarU from '../components/NavBarU';
+import React, { Component } from "react";
+import NavBarU from "../components/NavBarU";
 
 export default class Ranking extends Component {
-    render() {
-        return (
-            <div>
-                <NavBarU />
-                <h1>Ranking</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <NavBarU />
+        <h1>Ranking</h1>
+      </div>
+    );
+  }
 }
